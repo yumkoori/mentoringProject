cba5576dbcb24840649d74d8cdbd6dbf08551259
yumi <yumkoori@gmail.com>
2024-09-02 21:48:15 +0900

    Feat: 도메인 영역과 영속성 계층간의 모델 매핑 클래스를 추가

---
+++ src/main/java/com/yumkoori/mentoring/user/adapter/out/persistence/PersistenceMapper.java
@@ -0,0 +1,22 @@
+package com.yumkoori.mentoring.user.adapter.out.persistence;
+
+import com.yumkoori.mentoring.user.adapter.out.persistence.entity.EmailVerificationJpaEntity;
+import com.yumkoori.mentoring.user.domain.EmailVerification;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Getter
+public class PersistenceMapper {
+
+    public static EmailVerificationJpaEntity mapToVerificationJpaEntity(EmailVerification verificationDomain) {
+        return new EmailVerificationJpaEntity(verificationDomain.getEmail(),
+                verificationDomain.getVerification(), verificationDomain.getStatus());
+    }
+
+    public static EmailVerification mapToVerificationDomain(EmailVerificationJpaEntity verificationJpaEntity) {
+        return new EmailVerification(verificationJpaEntity.getEmail(), verificationJpaEntity.getVerification(),verificationJpaEntity.getStatus());
+    }
+}
