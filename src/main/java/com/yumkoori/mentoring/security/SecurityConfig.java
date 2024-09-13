3ee3a60b3235af869a29d5322f22904b09ce843a
yumi <yumkoori@gmail.com>
2024-09-13 22:37:27 +0900

    Refactor: securityConfig에 password 암호화를 위한 빈 추가
    - 보안을 높이기 위해 회원정보를 저장할때 password 암호화 추가
    - 높은 보안성을과 사용이 간단한 BCypt 암호화 알고리즘 선택

--- src/main/java/com/yumkoori/mentoring/security/SecurityConfig.java
+++ src/main/java/com/yumkoori/mentoring/security/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -48,6 +50,10 @@ public class SecurityConfig {
         return source;
 
     }
-    
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
