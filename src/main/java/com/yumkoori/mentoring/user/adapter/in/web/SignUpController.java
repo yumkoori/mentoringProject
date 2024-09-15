3348602f63f9be8e634e78d1564aaae7210cb6c6
yumi <yumkoori@gmail.com>
2024-09-15 19:18:49 +0900

    Feat: 회원가입 controller 구현

---
+++ src/main/java/com/yumkoori/mentoring/user/adapter/in/web/SignUpController.java
@@ -0,0 +1,39 @@
+package com.yumkoori.mentoring.user.adapter.in.web;
+
+import com.yumkoori.mentoring.common.ResultDto;
+import com.yumkoori.mentoring.common.WebAdapter;
+import com.yumkoori.mentoring.user.adapter.in.web.dto.SignupRequestForEmailDto;
+import com.yumkoori.mentoring.user.application.port.in.RegisterUserUseCase;
+import com.yumkoori.mentoring.user.application.port.in.command.RegisterUserCommand;
+import com.yumkoori.mentoring.user.application.port.in.command.RegisterUserCommand.RegisterUserCommandBuilder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@WebAdapter
+@RestController
+@RequiredArgsConstructor
+public class SignUpController {
+
+    private final RegisterUserUseCase registerUserUseCase;
+
+    @PostMapping("/auth/signup")
+    public ResponseEntity<ResultDto<String>> signup_Email(@RequestBody SignupRequestForEmailDto request) {
+        RegisterUserCommand registerCommand = RegisterUserCommand.builder()
+                .email(request.getEmail())
+                .password(request.getPassword())
+                .nick(request.getNick())
+                .build();
+
+        registerUserUseCase.registerUser(registerCommand);
+
+        ResultDto<String> result = new ResultDto<>(200, "회원가입 완료", registerCommand.getNick());
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
+
+    }
+
+}
