eaf1609bce980a589e74d0ba72a2f8c57a3fdd54
yumi <yumkoori@gmail.com>
2024-09-26 23:10:32 +0900

    Refactor: 사용자 인증처리를 위한 SecurityConfig 추가 및 수정

--- src/main/java/com/yumkoori/mentoring/security/SecurityConfig.java
+++ src/main/java/com/yumkoori/mentoring/security/SecurityConfig.java
@@ -1,17 +1,23 @@
 package com.yumkoori.mentoring.security;
 
+import com.yumkoori.mentoring.user.application.port.service.CustomUserDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -22,18 +28,48 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final CustomUserDetailService customUserDetailService;
+
+    //스프링 시큐리티 비활성화
     @Bean
-    protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring().requestMatchers("/static/**", "/css/**", "/js/**");
+    }
+
+
+    //특정 HTTP요청에 대한 웹 기반 보안 구성
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                new AntPathRequestMatcher("/auth/**")
+                        ).permitAll()
+                        .requestMatchers("/main").hasAnyAuthority("MENTEE")
+                        .anyRequest().authenticated())
+
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/auth/login")
+                        .defaultSuccessUrl("/main")
+                )
                 .cors(cors -> cors
                         .configurationSource(corsConfigurationSource())
                 ).csrf(CsrfConfigurer::disable)
                 .httpBasic(HttpBasicConfigurer::disable)
                 .sessionManagement(sessionManagement -> sessionManagement
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                 );
 
                 return httpSecurity.build();
+    }
+
+    //인증 관리자 관련 설정
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity, PasswordEncoder passwordEncoder) throws Exception {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(customUserDetailService);
+        authProvider.setPasswordEncoder(passwordEncoder);
+        return new ProviderManager(authProvider);
     }
 
     @Bean
