8a9d2f80945f3b661c0c4777660f1e374a18b419
yumi <yumkoori@gmail.com>
2024-09-26 23:01:21 +0900

    Feat: 사용자 인증정보를 가져오기 위한 시큐리티 UserDetails, DetailService 구현
    - 시큐리티에서 사용자의 인증정보를 담아두기 위한 인터페이스인 UserDetails를 상속 후 구현
    - 시큐리티에서 사용자 정보를 가져오기위해 필요한 인터페이스 UserDetailsService 상속 후 구현

---
+++ src/main/java/com/yumkoori/mentoring/user/domain/CustomUserDetails.java
@@ -0,0 +1,55 @@
+package com.yumkoori.mentoring.user.domain;
+
+import java.util.Collection;
+import java.util.List;
+import lombok.Builder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Builder
+public class CustomUserDetails implements UserDetails {
+
+    private final String email;
+    private final String password;
+
+    public CustomUserDetails(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("MENTEE"));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
