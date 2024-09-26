c199e0f7674af757a35730533ab29a9acda8573e
yumi <yumkoori@gmail.com>
2024-09-26 22:48:46 +0900

    Feat: 사용자 조회를 위한 findByEmail 리포지터리 구현

--- src/main/java/com/yumkoori/mentoring/user/adapter/out/persistence/SpringDataUserRepository.java
+++ src/main/java/com/yumkoori/mentoring/user/adapter/out/persistence/SpringDataUserRepository.java
@@ -1,10 +1,13 @@
 package com.yumkoori.mentoring.user.adapter.out.persistence;
 
 import com.yumkoori.mentoring.user.adapter.out.persistence.entity.UserJpaEntity;
+import com.yumkoori.mentoring.user.domain.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SpringDataUserRepository extends JpaRepository<UserJpaEntity, Long> {
 
     boolean existsByEmail(String email);
 
+    Optional<UserJpaEntity> findByEmail(String email);
 }
