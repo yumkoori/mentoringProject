cfa113cbc9ff5e7ddeafa7b81e2bfb47415ae463
yumi <yumkoori@gmail.com>
2024-09-27 20:56:22 +0900

    Feat: jwt서비스를 사용하기 위한 의존성 추가 및 설정 추가
    - properties에 설정한 이슈 발급자와 비밀키에 접근하기 위한 매핑 클래스 추가

---
+++ src/main/java/com/yumkoori/mentoring/config/JwtProperties.java
@@ -0,0 +1,13 @@
+package com.yumkoori.mentoring.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties("jwt")
+public class JwtProperties {
+    private String issuer;
+    private String secretKey;
+}
